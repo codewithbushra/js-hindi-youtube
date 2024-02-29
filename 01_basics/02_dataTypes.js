cd602ae6dc928880f855d634611255d27d742000
Code With Bushra <148866067+codewithbushra@users.noreply.github.com>
2024-02-29 13:49:18 +0000

    update

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,5 +5,25 @@
 console.log(3+3);
 
  //readability should be high
- 
+
 console.log(4+4);
+
+let name = "Bushra"
+let age = 24
+let userLogedIn = true //or false
+
+//DataTypes
+
+// number => 2 to power 30
+// BigInt
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined
+// symbol => unique 
+
+// object
+
+console.log(typeof "Bushra" );
+console.log(typeof indefined); //type will be undefined
+console.log(typeof null); // type will be object
