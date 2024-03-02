ab7dd6f63e8b26c9c011644d620a297a05c1b42b
Code With Bushra <148866067+codewithbushra@users.noreply.github.com>
2024-03-02 15:33:56 +0000

    update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,27 @@
-let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+let score = true
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// let useuLoggedIn = 1
+// let useuLoggedIn = 0
+// let useuLoggedIn = ""
+let useuLoggedIn = "Bushra Anwar"
+let booleanuserLoggedIn = Boolean(useuLoggedIn)
+
+console.log(booleanuserLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Bushra Anwar" => true
