66e4346940595c496323c9c98ee579f08ca34bff
Code With Bushra <148866067+codewithbushra@users.noreply.github.com>
2024-03-04 13:37:56 +0000

    update

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+ 
+
+// console.log("2" > 1);  //=> data type should be same
+// console.log("02" > 1);  //=> data type should be same
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
