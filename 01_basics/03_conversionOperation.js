d59a879453cdac4b2b2cfa6de7a49aabdd599937
Code With Bushra <148866067+codewithbushra@users.noreply.github.com>
2024-03-04 13:26:22 +0000

    update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,7 +53,18 @@ let str3 = str1 + str2
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
-//console.log(+true);
-//console.log(true+);
-//console.log(+"");
+//console.log(+true); //=>1
+//console.log(true+); //=>Error
+//console.log(+""); //=>0
 
+let num1 ,num2 ,num3 
+num1 ,num2 ,num3 = 2+2
+
+let gameCounter = 100
+gameCounter++; // => postfix
+++gameCounter; // => prefix 
+console.log(gameCounter);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
