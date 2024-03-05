05aa96f1eea928e88d951686d706dc1e0a297b46
Code With Bushra <148866067+codewithbushra@users.noreply.github.com>
2024-03-05 14:49:42 +0000

    update

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -30,6 +30,29 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+/**********************Memory***************************/
+
+//stack (primitive) ,Heap (Non Primitive)
+
+let myYoutubename = "Code with Bushra"
+let anotherName = myYoutubename
+
+anotherName = "Bushra Anwar Vlog"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+   email : "codewithbushra@gmail.com",
+   mob : +9212345678
+}
+let userTwo = userOne
+
+userTwo.email = "bushranwar@gmail.com"
+console.log(userTwo.email);
+console.log(userOne.email);
